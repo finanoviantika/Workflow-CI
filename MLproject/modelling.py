5d186de3eaa936e120a59c4388ae9503c0cb6415
finanoviantika <74105532+finanoviantika@users.noreply.github.com>
2025-05-12 13:51:36 +0700

    Update modelling.py

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -4,38 +4,43 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(40)
 
-mlflow.set_experiment("Students Habit Performance")
-
-data = pd.read_csv('dataset_preprocessing.csv')
-
-X = data.drop("exam_score", axis=1)
-y = data["exam_score"]
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
-with mlflow.start_run():
-    #Log Parameters
-    n_estimators = 100
-    max_depth = 5
-    mlflow.autolog()
-
-    #Train Model
-    model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth)
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path = "model",
-    )
-
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-
-    #Log Metrics
-    r2_Score = r2_score(y_test, y_pred)
-    rmse = mean_squared_error(y_test, y_pred)
+    mlflow.set_tracking_uri("http://127.0.0.1:5000/")
     
-    mlflow.log_metric("r2_score", r2_Score)
-    mlflow.log_metric("rmse", r2_Score)
+    mlflow.set_experiment("Students Habit Performance")
+    
+    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "dataset_preprocessing.csv")
+    data = pd.read_csv(file_path)
+    
+    X = data.drop("exam_score", axis=1)
+    y = data["exam_score"]
+    
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    
+    with mlflow.start_run():
+        #Log Parameters
+        n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
+        max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 5
+        mlflow.autolog()
+    
+        #Train Model
+        model = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth)
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path = "model",
+        )
+    
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+    
+        #Log Metrics
+        r2_Score = r2_score(y_test, y_pred)
+        rmse = mean_squared_error(y_test, y_pred)
+        
+        mlflow.log_metric("r2_score", r2_Score)
+        mlflow.log_metric("rmse", r2_Score)
